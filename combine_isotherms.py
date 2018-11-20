936443670ad06de4349d9e75075d7846e6e3997c
gchachra <gaurav@siluriatech.com>
2018-11-20 13:24:32 -0800

    Ability to process only the files that are not already in all_isotherms.csv

--- combine_isotherms.py
+++ combine_isotherms.py
@@ -5,19 +5,12 @@ all_files = os.listdir('.\\output\\')
 
 df_all_isotherms = pd.read_csv('all_isotherms.csv')
 
-# print(df_all_isotherms['filename'].str.contains('hello').any())
-
 for item in all_files:
-    # df_all_isotherms['filename'].str.contains(item.split('.')[0], regex=False).to_csv(item+'bool.csv')
 
-
-    if df_all_isotherms['Filename'].str.contains(item.split('.')[0], regex=False).any() == True:
+    if df_all_isotherms['Filename'].str.contains(item.split('.')[0], regex=False).any():
         pass
     else:
         df = pd.read_csv('.\\output\\'+item)
         df_all_isotherms = df_all_isotherms.append(df, ignore_index=True)
 
-#     if item.split('.')[0] in df_all_isotherms['filename'] == True:
-#         print('True')
-#
 df_all_isotherms.to_csv('all_isotherms.csv', index=False)
